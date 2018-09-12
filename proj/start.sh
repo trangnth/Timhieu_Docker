31d1f897fd44380b0929d3bfd0cfc3a9fd9627aa
trangnth <nguyenhuyentrang1996@gmail.com>
2018-09-12 16:30:25 +0700

    update

---
+++ proj/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install packages
+apt-get update
+apt-get install python3 python3-pip -y
+apt-get install libmysqlclient-dev -y
+apt-get install git -y
+
+# Clone project
+#if [ ! -d "backup-mgmt" ]; then
+git clone -b dev https://trangnth@github.com/locvx1234/backup-mgmt.git
+#fi
+
+# Install requirements
+cd backup-mgmt/
+pip3 install --upgrade pip
+pip install -r requirements.txt
+
+# Run server
+cd backup
+python3 manage.py runserver 0.0.0.0:8008
+
+exec $@
